60c99adbccff23524190c70b53c0c4ac1edfff81
Jason Maradiaga <jasonmaradiaga27@gmail.com>
2025-02-10 23:28:54 -0600

    objects-ternarios-arrays-others

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ console.log (completeName);
 //arrays 
 const array = ['text', 123, 151];
 
+console.log(array);
+console.log(array[0]);
+
+const people = [];
+people[0] = 'Dennis';
+people[1] = 'Daniel';
+people[2] = 'Moises';
+people[3] = 'Brandon';
+people[5] = 'Gerardo';
+
 //object
 const customers = {
     firstName: 'Diana',
@@ -27,15 +37,28 @@ const customers = {
     car: {
         model: 'Skyline',
         brand: "Nissan"
+    },
+    saludo: function (){
+        alert('hola');
     }
 }
 
 console.log(customers.car.model);
+console.log(customers['firstName']);
+
+let name1 = 'Jason';
+console.log(customers[name1]);
+
+customers.country = 'Honduras'; //add propierties to the object
+console.log (customers);
+
+console.log(`hi mi name is ${customers.firstName}`) //To add text strings with variables using 'back ticks'
 
 //functions
 function printHello(){
     console.log("hola");
 }
+customers.saludo();
 
 printHello();
 
@@ -52,3 +75,16 @@ const variable2 = undefined;
 //module
 const variableModule = 17 % 3;
 console.log(variableModule);
+
+/*switch cases structure
+=============================*/
+switch(customers.country){
+    case 'Honduras':
+        console.log('To be from Honduras');
+}
+
+/*ternario
+=============================*/
+const ticket = 'vip';
+const accessCode = ticket === 'vip' ? 'VIP-123-456' : 'REGULAR-456-789'; //if (=) variable = condition then (?) '..../code' else (:) '...../code' 
+console.log (accessCode);
